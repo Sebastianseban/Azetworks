f0c7302484de9878ce1167df8a023a4950add816
sebastian <sebastianseban0105@gmail.com>
2026-05-16 12:28:52 +0530

    feat: configure SEO metadata and implement robots.txt and sitemap.xml for site indexing

---
+++ src/app/sitemap.js
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  const baseUrl = "https://www.azetworks.com";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/Solutions`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/partners`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
